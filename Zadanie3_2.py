90848e6c31d0b9d67d508efb4a8d9b8953af0257
Piotr Andruchowski <andrupio@gmail.com>
2021-02-21 21:14:17 +0100

    Add sklep zoologiczny key and values to the dictionary

--- Zadanie3_2.py
+++ Zadanie3_2.py
@@ -1,7 +1,8 @@
 print("Zadanie 1")
 shopping = {
     "piekarnia": ["chleb", "bułki", "pączek", "ptyś", "bułka słodka"],
-    "sklep mobilny": ["marchew", "seler", "rukola"]
+    "sklep mobilny": ["marchew", "seler", "rukola"],
+    "sklep zoologiczny": ["żółw czerwonolicy", "chomik", "papuga"]
 }
 for key, value in shopping.items():
     print(f"Idę do", key.capitalize(),
