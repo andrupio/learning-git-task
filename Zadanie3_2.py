4ace97b95940a78f6cc2a28c5a1046ffe1d69492
Piotr Andruchowski <andrupio@gmail.com>
2021-02-23 20:06:15 +0100

    Add Special greetings to the Mentor

--- Zadanie3_2.py
+++ Zadanie3_2.py
@@ -8,3 +8,4 @@ for key, value in shopping.items():
     print(f"Idę do", key.capitalize(),
           "kupuję tu następujące rzeczy:", str(value).title())
 print(f"W sumie kupuję", sum(len(i) for i in shopping.values()), "produktów")
+print("Special greetings to the Mentor")
