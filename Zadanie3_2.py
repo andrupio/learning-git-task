cd3bc48c54bf46bf26de0ce0d5000a935aea378c
Piotr Andruchowski <andrupio@gmail.com>
2021-02-24 16:39:25 +0100

    Add print

--- Zadanie3_2.py
+++ Zadanie3_2.py
@@ -9,3 +9,4 @@ for key, value in shopping.items():
     print(f"Idę do", key.capitalize(),
           "kupuję tu następujące rzeczy:", str(value).title())
 print(f"W sumie kupuję", sum(len(i) for i in shopping.values()), "produktów")
+print()
